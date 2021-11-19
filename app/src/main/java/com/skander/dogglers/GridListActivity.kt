fd27abff36a365df466cb68e819d5cd3b54f26ff
Skander CHOUK <choukskander1@gmail.com>
2021-11-20 00:16:29 +0100

    Initial commit

---
+++ app/src/main/java/com/skander/dogglers/GridListActivity.kt
@@ -0,0 +1,29 @@
+package com.skander.dogglers
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.skander.dogglers.adapter.DogCardAdapter
+import com.skander.dogglers.const.Layout
+import com.skander.dogglers.databinding.ActivityGridListBinding
+
+class GridListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGridListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGridListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.gridRecyclerView.adapter = DogCardAdapter(
+            applicationContext,
+            Layout.GRID
+        )
+
+        // Specify fixed size to improve performance
+        binding.gridRecyclerView.setHasFixedSize(true)
+
+        // Enable up button for backward navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
---
+++ app/src/main/java/com/skander/dogglers/HorizontalListActivity.kt
@@ -0,0 +1,29 @@
+package com.skander.dogglers
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.skander.dogglers.adapter.DogCardAdapter
+import com.skander.dogglers.const.Layout
+import com.skander.dogglers.databinding.ActivityHorizontalListBinding
+
+class HorizontalListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHorizontalListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHorizontalListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.horizontalRecyclerView.adapter = DogCardAdapter(
+            applicationContext,
+            Layout.HORIZONTAL
+        )
+
+        // Specify fixed size to improve performance
+        binding.horizontalRecyclerView.setHasFixedSize(true)
+
+        // Enable up button for backward navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
